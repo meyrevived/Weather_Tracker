6b72ea8fea4cdc06b424788cebd0c30b021734fa
meyrevived <84704676+meyrevived@users.noreply.github.com>
2021-06-12 11:38:22 +0300

    Upgrades and improvements

--- Collector.py
+++ Collector.py
@@ -4,49 +4,72 @@ Collecting data from Current Weather and loads it into CityWeather list
 """
 import requests
 from datetime import datetime
+from multiprocessing import Process
 
-"""
-RESP API function
-Takes city ID.
-Returns a dictionary with the weather data.
-"""
+weather_data = {}
+network_error = False
 
 
-def get_current_city_weather(city_id):
-    weather_data = {"time": datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), "threshold_met": False}
+def get_current_city_weather(city_id, stop, run_api_key):
+    """
+    RESP API function with ability to stop Weather Tracker is connection issues are experienced.
+    Takes city ID, Stop and ApiKey.
+    Appends weather data to weather_data.
+    """
 
-    resp = requests.get(
-        'https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={API_key}&units=metric'.format(city_id=city_id, API_key="e735b6b632e6c008be941b8dbdb346d4"))
+    try:
+        weather_data["time"] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        weather_data["threshold_met"] = False
 
-    if resp.status_code == 429:
-        print("OpenWeather database is experiencing too many requests from this machine, please try running "
-              "Weather Tracker at another time")
+        resp = requests.get(
+            'https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={API_key}&units=metric'
+            ''.format(city_id=city_id, API_key=run_api_key))
+
+        if resp.status_code == 429:
+            print("OpenWeather database is experiencing too many requests from this machine, please try running "
+                  "Weather Tracker at another time")
+            stop.stop_all = True
+        if resp.status_code != 200:
+            print("Cannot fetch this city's weather, received status code {}".format(resp.status_code))
+            return
+
+        response = resp.json()
+        weather_data["city_name"] = response["name"]
+        weather_data["temperature"] = response["main"]["temp"]
+        weather_data["wind_speed"] = response["wind"]["speed"]
+
+    except Exception as e:
+        print("Weather Tracker is experiencing {err}".format(err=e.__class__.__name__))
+        print("Please fix connection issues in your machine and try Weather Tracker again")
+        stop.say_goodbye()
         exit()
-    if resp.status_code != 200:
-        print("Cannot fetch this city's weather, received status code {}".format(resp.status_code))
-        return {}
-
-    response = resp.json()
-    weather_data["city_name"] = response["name"]
-    weather_data["temperature"] = response["main"]["temp"]
-    weather_data["wind_speed"] = response["wind"]["speed"]
-
-    return weather_data
 
 
-"""
-Gets the needed data from RESP API function and loads it into CityWeather map.
-Takes CityWeather and REST API data.
-Returns True.
-"""
+def load_city_weather(city_weather, city_id, stop, run_api_key):
+    """
+    Gets the needed data from RESP API function and loads it into CityWeather map.
+    Takes CityWeather, city_id, Stop and ApiKey.
+    Runs get_current_city_weather with a timeout to escape connection issues.
+    Attempts collecting weather data five times before declaring that collection is importable.
+    Returns True or False depending on its successful status.
+    """
+    failed_runs = 0
 
+    collection_process = Process(get_current_city_weather(city_id, stop, run_api_key))
+    collection_process.start()
+    collection_process.join(120)
+    if collection_process.is_alive():
+        print("Connection to OpenWeather database is taking too long, terminating collection processes")
+        collection_process.terminate()
+        stop.stop_all = True
+        collection_process.join()
 
-def load_city_weather(city_weather, city_id):
-    new_weather = get_current_city_weather(city_id)
+    while failed_runs < 5 or not stop.stop_all:
+        if weather_data:
+            city_weather.put_data(weather_data)
+            return True
+        else:
+            print("Error fetching city weather for {city}".format(city=city_id))
+            failed_runs += 1
 
-    if new_weather:
-        city_weather.put_data(new_weather)
-        return True
-    else:
-        print("Error fetching city weather")
-        return False
+    return False
